cd2aa9d767a09375af8b99231a5f44228c1bb3d9
Thorben von Hacht <thorben@vh8.de>
2018-01-14 13:42:27 -0800

    Add comments and empty lines to method

--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,7 @@
 "use strict";
 
-//const pegjsJava = require("pegjs-java");
-const pegjsJava = require("../../pegjs-java/pegjs-java");
+const pegjsJava = require("pegjs-java");
+//const pegjsJava = require("../../pegjs-java/pegjs-java");
 
 function parse(text) {
   /*parse(text, parsers, opts)*/
