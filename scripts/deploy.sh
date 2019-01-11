8c784834d1a16a19fc2c422b179953444f095f2c
Shahar Soel <shahars@protonmail.com>
2019-01-11 11:31:17 +0200

    "git status" as debug command for release process on travis.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,6 @@
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+# lerna would fail if there are uncommited files in the git working tree
+# but it won't tell us what are the files, the "git status" command will help
+# debug CI failures (e.g in travis)
+git status
 yarn run lerna-publish
