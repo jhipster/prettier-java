a5b8a4ee06e0f6210cd6f1a24a465cda9ea8e92c
Clément Dessoude <clement.dessoude@gmail.com>
2019-03-20 15:05:19 +0100

    indent modules declarations

--- packages/prettier-plugin-java/src/printers/packages-and-modules.js
+++ packages/prettier-plugin-java/src/printers/packages-and-modules.js
@@ -1,7 +1,7 @@
 "use strict";
 /* eslint-disable no-unused-vars */
 
-const { concat, join, line, ifBreak, group } = require("prettier").doc.builders;
+const { concat, indent, join, line } = require("prettier").doc.builders;
 const { buildFqn, rejectAndJoin, rejectAndConcat } = require("./printer-utils");
 
 class PackagesAndModulesPrettierVisitor {
@@ -76,8 +76,9 @@ class PackagesAndModulesPrettierVisitor {
       optionalOpen,
       "module",
       name,
-      // TODO: fix indentation
-      rejectAndJoin(line, ["{", join(line, moduleDirectives), "}"])
+      indent(rejectAndJoin(line, ["{", join(line, moduleDirectives)])),
+      line,
+      "}"
     ]);
   }
 
