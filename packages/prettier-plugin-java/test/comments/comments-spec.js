3022e1ca5c28d4a3df01748f5a3d76ed05be00ca
lc-ippon <lingchun.so@gmail.com>
2019-04-30 11:52:45 +0200

    add tests for comment print

--- packages/prettier-plugin-java/test/comments/comments-spec.js
+++ packages/prettier-plugin-java/test/comments/comments-spec.js
@@ -2,6 +2,7 @@ const { testSample } = require("../test-utils");
 const path = require("path");
 
 describe("prettier-java", () => {
+  testSample(path.resolve(__dirname, "./class"));
   testSample(path.resolve(__dirname, "./edge"));
   testSample(path.resolve(__dirname, "./interface"));
   testSample(path.resolve(__dirname, "./package"));
