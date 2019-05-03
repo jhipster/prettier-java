72a8defc1aa9a152b91eb841dd4230c4b5a67f5a
Ling-Chun SO <lingchun.so@gmail.com>
2019-05-03 17:37:54 +0200

    refactor update-test-output

--- packages/prettier-plugin-java/scripts/update-test-output.js
+++ packages/prettier-plugin-java/scripts/update-test-output.js
@@ -2,21 +2,37 @@
 "use strict";
 const klawSync = require("klaw-sync");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs-extra");
 const prettier = require("prettier");
 
-const samplesDir =
-  process.argv.indexOf("-single") > -1
-    ? path.resolve(__dirname, "./single-printer-run")
-    : path.resolve(__dirname, "../test");
+let samplesDir = path.resolve(__dirname, "../test");
+let originalSamplesDir = samplesDir;
+if (process.argv.indexOf("-single") > -1) {
+  samplesDir = path.resolve(__dirname, "./single-printer-run");
+} else if (process.argv.indexOf("-repository") > -1) {
+  const sample = path.resolve(__dirname, "../sample");
+  originalSamplesDir = path.resolve(__dirname, process.argv[3]);
+  samplesDir = path.resolve(sample, path.basename(originalSamplesDir));
+  if (fs.existsSync(samplesDir)) {
+    fs.removeSync(samplesDir);
+  }
+  console.log(`start copy ${originalSamplesDir} to ${samplesDir}`);
+  fs.copySync(originalSamplesDir, samplesDir);
+  console.log(`end copy ${originalSamplesDir} to ${samplesDir}`);
+}
 
 const sampleFiles = klawSync(samplesDir, { nodir: true });
-const javaSampleFiles = sampleFiles.filter(fileDesc =>
-  fileDesc.path.endsWith("input.java")
-);
+const javaSampleFiles = sampleFiles.filter(fileDesc => {
+  if (fileDesc.path.includes("node_modules")) {
+    return false;
+  }
+  if (process.argv.indexOf("-repository") > -1) {
+    return fileDesc.path.endsWith(".java");
+  }
+  return fileDesc.path.endsWith("input.java");
+});
 
 let failures = 0;
-
 javaSampleFiles.forEach(fileDesc => {
   const javaFileText = fs.readFileSync(fileDesc.path, "utf8");
 
@@ -27,16 +43,20 @@ javaSampleFiles.forEach(fileDesc => {
       plugins: [path.resolve(__dirname, "../")],
       tabWidth: 2
     });
-    const outputFilePath = fileDesc.path.replace(/input.java$/, "output.java");
+    let outputFilePath = fileDesc.path.replace(/input.java$/, "output.java");
+    if (process.argv.indexOf("-repository") > -1) {
+      outputFilePath = fileDesc.path;
+    }
     console.log(`writing <${outputFilePath}>`);
     fs.writeFileSync(outputFilePath, newExpectedText);
   } catch (e) {
     failures++;
     console.log(`Failed parsing: <${fileDesc.path}>`);
-    console.log(e.message);
+    console.log(e);
   }
 });
 
 if (failures > 0) {
+  console.log(`fail: ${failures}`);
   process.exit(1);
 }
