2e017c967697735fc7afdf1bcaec229516a22274
Ling-Chun SO <lingchun.so@gmail.com>
2019-05-07 18:14:32 +0200

    removing some test repositories to reduce ci time

--- packages/prettier-plugin-java/test/prettify-test.js
+++ packages/prettier-plugin-java/test/prettify-test.js
@@ -2,24 +2,7 @@
 const { resolve } = require("path");
 const { testRepositorySample } = require("./test-utils");
 
-const jhipsterRepository = [
-  "jhipster",
-  "jhipster-online",
-  "jhipster-sample-app",
-  "jhipster-sample-app-microservice",
-  "jhipster-sample-app-gateway",
-  "jhipster-sample-app-oauth2",
-  "jhipster-sample-app-websocket",
-  "jhipster-sample-app-noi18n",
-  "jhipster-sample-app-nocache",
-  "jhipster-sample-app-hazelcast",
-  "jhipster-sample-app-elasticsearch",
-  "jhipster-sample-app-dto",
-  "jhipster-sample-app-couchbase",
-  "jhipster-sample-app-cassandra",
-  "jhipster-sample-app-mongodb",
-  "jhipster-sample-app-react"
-];
+const jhipsterRepository = ["jhipster", "jhipster-sample-app"];
 
 describe("prettier-java", () => {
   testRepositorySample(
@@ -42,10 +25,4 @@ describe("prettier-java", () => {
       ["compile"]
     );
   });
-
-  testRepositorySample(
-    resolve(__dirname, `../samples/jhipster-sample-app-gradle`),
-    "./gradlew",
-    ["build"]
-  );
 });
