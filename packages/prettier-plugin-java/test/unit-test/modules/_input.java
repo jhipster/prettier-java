70aac21d289b53541bb73c91f882bb08a58cc114
Ling-Chun SO <lingchun.so@gmail.com>
2019-06-26 15:23:06 +0200

    fix modules print

---
+++ packages/prettier-plugin-java/test/unit-test/modules/_input.java
@@ -0,0 +1,22 @@
+open module soat.vending.machine.gui {
+  requires java.desktopa ;
+  requires soat.vending.machine.model;
+  requires transitive soat.core;
+   exports  fr.soat.vending.machine.model to another , again, ano;
+
+   exports  fr.soat.vending.machine.model to another , again, ano, waht, another , again, ano, averyveryveryveryveryveryveryveryveryveryverylongname;
+
+
+   opens  fr.soat.vending.machine.model to another , again,ano ;
+
+   opens  fr.soat.vending.machine.model to another , again,ano,another , again,ano,another , again,ano,another , again,averyveryveryveryveryveryveryveryveryveryverylongname ;
+
+
+
+  uses fr.soat.vendinga.machine.services.DrinksService ;
+
+  provides  model with another , again,ano ;
+
+  provides  model with another , again,ano,another , again,ano,another , again,ano,another , again,averyveryveryveryveryveryveryveryveryveryverylongname ;
+  
+}
