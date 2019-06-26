4020d74f8e8f86271bf31db898351838babf072d
Ling-Chun SO <lingchun.so@gmail.com>
2019-06-26 14:34:14 +0200

    add tests bug fix #219

--- packages/prettier-plugin-java/test/unit-test/comments/class/_input.java
+++ packages/prettier-plugin-java/test/unit-test/comments/class/_input.java
@@ -774,4 +774,14 @@ public final class ArrayTable<R, C, V> extends AbstractTable<R, C, V> implements
   }
 
   private static final long serialVersionUID = 0;
+
+
+  @Override
+  public Optional<String> getCurrentAuditor() {
+    // There is currently no reactive AuditorAware implementation so we can't
+    // extract the currently logged-in user from the Reactor Context.
+    // Therefore createdBy and lastModifiedBy will have to be set explicitly.
+    // See https://jira.spring.io/browse/DATACMNS-1231
+    return Optional.of(Constants.SYSTEM_ACCOUNT);
+  }
 }
