bdf9ddb1df9c2cb626465c4ceeac35095d648841
Ling-Chun SO <lingchun.so@gmail.com>
2019-06-26 16:27:13 +0200

    add test case for module declaration

--- packages/prettier-plugin-java/test/unit-test/modules/_input.java
+++ packages/prettier-plugin-java/test/unit-test/modules/_input.java
@@ -4,10 +4,14 @@ open module soat.vending.machine.gui {
   requires transitive soat.core;
    exports  fr.soat.vending.machine.model to another , again, ano;
 
+   exports  fr.soat.vending.machine.model.it.should.be.breaking.but.only.a.part to another , again, ano;
+
    exports  fr.soat.vending.machine.model to another , again, ano, waht, another , again, ano, averyveryveryveryveryveryveryveryveryveryverylongname;
 
 
    opens  fr.soat.vending.machine.model to another , again,ano ;
+
+   opens  fr.soat.vending.machine.model.it.should.be.breaking.but.only.a.part to another , again,ano ;
 
    opens  fr.soat.vending.machine.model to another , again,ano,another , again,ano,another , again,ano,another , again,averyveryveryveryveryveryveryveryveryveryverylongname ;
 
@@ -16,6 +20,8 @@ open module soat.vending.machine.gui {
   uses fr.soat.vendinga.machine.services.DrinksService ;
 
   provides  model with another , again,ano ;
+
+  provides  fr.soat.vending.machine.model.it.should.be.breaking.but.only.a.part with another , again,ano ;
 
   provides  model with another , again,ano,another , again,ano,another , again,ano,another , again,averyveryveryveryveryveryveryveryveryveryverylongname ;
   
