31cfd1d25f07f046b27856b04250e97ce677d836
Clément Dessoude <clement.dessoude@gmail.com>
2019-10-01 16:59:29 +0200

    add unit tests for imports

---
+++ packages/prettier-plugin-java/test/unit-test/package_and_imports/classWithOnlyNonStaticImports/_output.java
@@ -0,0 +1,10 @@
+package my.own.pkg;
+
+import abc.def;
+import abc.def.Another;
+import abc.def.Something;
+import java.utils.*;
+import one.last;
+import something.Different;
+
+public class PackageAndImports {}
---
+++ packages/prettier-plugin-java/test/unit-test/package_and_imports/classWithOnlyStaticImports/_output.java
@@ -0,0 +1,9 @@
+package my.own.pkg;
+
+import static abc.def;
+import static abc.def.Another;
+import static abc.def.Something;
+import static java.utils.*;
+import static something.Different;
+
+public class PackageAndImports {}
