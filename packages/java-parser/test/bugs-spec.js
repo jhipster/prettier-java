6c6d7703b88d59e7faed75f6c3f63f6d41800fef
Clément Dessoude <clement.dessoude@gmail.com>
2019-11-26 09:37:18 +0100

    refactor(tests): clean tests by removing unecessary describe sections

--- packages/java-parser/test/bugs-spec.js
+++ packages/java-parser/test/bugs-spec.js
@@ -3,14 +3,7 @@
 const { expect } = require("chai");
 const javaParser = require("../src/index");
 
-// TODO: we don't need to copy the describe section so many times in this file...
-//   `IT` should be nested under describe...
 describe("The Java Parser fixed bugs", () => {
-  it("issue #129 - this.<bar>.baz()", () => {
-    const input = "this.<Number>anyIterableType()";
-    expect(() => javaParser.parse(input, "expression")).to.not.throw();
-  });
-
   it("issue #112 - Special handling of transitive keyword in module requires statement", () => {
     const input = `module foo {
       // Regular requires statement (without modifier)
@@ -23,39 +16,7 @@ describe("The Java Parser fixed bugs", () => {
     `;
     expect(() => javaParser.parse(input)).to.not.throw();
   });
-});
 
-describe("The Java Parser fixed bugs", () => {
-  it("issue #131 - 1.0e-10", () => {
-    const input = "1.0e-10";
-    expect(() => javaParser.parse(input, "expression")).to.not.throw();
-  });
-});
-
-describe("The Java Parser fixed bugs", () => {
-  it("issue #130 - enumConstantList", () => {
-    const input = "enum Foo { BAR, }";
-    expect(() => javaParser.parse(input)).to.not.throw();
-  });
-});
-
-describe("The Java Parser fixed bugs", () => {
-  it("issue #158 - semicolons inside imports", () => {
-    const input = "import Foo;;import Bar;";
-    expect(() =>
-      javaParser.parse(input, "ordinaryCompilationUnit")
-    ).to.not.throw();
-  });
-});
-
-describe("The Java Parser fixed bugs", () => {
-  it("issue #177 - annotation in packageDeclaration", () => {
-    const input = "@Test public class Foo{}";
-    expect(() => javaParser.parse(input)).to.not.throw();
-  });
-});
-
-describe("The Java Parser fixed bugs", () => {
   it("issue #113 - Bit shifting operators must not include whitespace - >>", () => {
     const input = "public class Foo{int hello = 1 >> 5;}";
     expect(() => javaParser.parse(input)).to.not.throw();
@@ -88,5 +49,32 @@ describe("The Java Parser fixed bugs", () => {
   it("issue #113 - Bit shifting operators must not include whitespace - space after first <", () => {
     const input = "public class Foo{int hello = 1 < < 5;}";
     expect(() => javaParser.parse(input)).to.throw();
+  });
+
+  it("issue #129 - this.<bar>.baz()", () => {
+    const input = "this.<Number>anyIterableType()";
+    expect(() => javaParser.parse(input, "expression")).to.not.throw();
+  });
+
+  it("issue #131 - 1.0e-10", () => {
+    const input = "1.0e-10";
+    expect(() => javaParser.parse(input, "expression")).to.not.throw();
+  });
+
+  it("issue #130 - enumConstantList", () => {
+    const input = "enum Foo { BAR, }";
+    expect(() => javaParser.parse(input)).to.not.throw();
+  });
+
+  it("issue #158 - semicolons inside imports", () => {
+    const input = "import Foo;;import Bar;";
+    expect(() =>
+      javaParser.parse(input, "ordinaryCompilationUnit")
+    ).to.not.throw();
+  });
+
+  it("issue #177 - annotation in packageDeclaration", () => {
+    const input = "@Test public class Foo{}";
+    expect(() => javaParser.parse(input)).to.not.throw();
   });
 });
