fc5390d640d884c2a17c1808229e0b2c53fa457c
Clement Dessoude <clement.dessoude@gmail.com>
2019-12-08 20:57:49 +0100

    fix test

--- packages/prettier-plugin-java/test/unit-test/prettier-ignore/prettier-ignore-spec.js
+++ packages/prettier-plugin-java/test/unit-test/prettier-ignore/prettier-ignore-spec.js
@@ -3,7 +3,6 @@ const path = require("path");
 
 describe("prettier-java", () => {
   testSample(path.resolve(__dirname, "./classDeclaration"));
-  testSample(path.resolve(__dirname, "./formatter-on-off"));
   testSample(path.resolve(__dirname, "./method"));
   testSample(path.resolve(__dirname, "./multiple-ignore"));
 });
