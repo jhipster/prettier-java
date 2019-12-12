3659894d07876bc162d85d204ce1e9a16c6d7f6d
jhaber <jhaber@hubspot.com>
2019-12-12 09:48:51 -0500

    Add another edge case

--- packages/prettier-plugin-java/test/unit-test/package_and_imports/classWithMixedCaseImports/_input.java
+++ packages/prettier-plugin-java/test/unit-test/package_and_imports/classWithMixedCaseImports/_input.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.function.Consumer;
+import java.util.function.ConsumerTwo;
 import java.util.List;
+import java.util.concurrent.*;
 import java.util.Map;
 
 public class PackageAndImports {}
--- packages/prettier-plugin-java/test/unit-test/package_and_imports/classWithMixedCaseImports/_output.java
+++ packages/prettier-plugin-java/test/unit-test/package_and_imports/classWithMixedCaseImports/_output.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.*;
 import java.util.function.Consumer;
+import java.util.function.ConsumerTwo;
 
 public class PackageAndImports {}
