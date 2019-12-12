e9b39beda3bc7cae1214564152ed0d6d166aede8
jhaber <jhaber@hubspot.com>
2019-12-12 09:45:42 -0500

    Sort imports naively

---
+++ packages/prettier-plugin-java/test/unit-test/package_and_imports/classWithMixedCaseImports/_input.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.List;
+import java.util.Map;
+
+public class PackageAndImports {}
---
+++ packages/prettier-plugin-java/test/unit-test/package_and_imports/classWithMixedCaseImports/_output.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class PackageAndImports {}
