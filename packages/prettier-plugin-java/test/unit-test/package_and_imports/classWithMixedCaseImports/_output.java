2b32449437db70e85f1fba6d46e7b58e05a937b6
jhaber <jhaber@hubspot.com>
2019-12-12 09:59:39 -0500

    More edge cases

--- packages/prettier-plugin-java/test/unit-test/package_and_imports/classWithMixedCaseImports/_output.java
+++ packages/prettier-plugin-java/test/unit-test/package_and_imports/classWithMixedCaseImports/_output.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
+import java.util.concurrent.Semaphore;
+import java.util.functioN.Consumer;
 import java.util.function.Consumer;
 import java.util.function.ConsumerTwo;
 
