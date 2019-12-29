7c6d8f748d74de22499c1770337e9f258370d28e
Clément Dessoude <clement.dessoude@gmail.com>
2019-12-29 13:18:34 +0100

    fix: indent comment based on the bloc they are in

---
+++ packages/prettier-plugin-java/test/unit-test/comments/comments-blocks-and-statements/end-of-block/_input.java
@@ -0,0 +1,133 @@
+class A {
+
+}
+
+class B {
+  // alpha
+}
+
+class C {
+  // alpha
+  // beta
+}
+
+class D {
+  // alpha
+  // beta
+  /* gamma */
+}
+
+class E { // alpha
+}
+
+class F {/* alpha */}
+
+class G {
+  /* alpha */
+  /* beta */
+}
+
+class H {
+  /* alpha */
+  // beta
+}
+
+
+class I { // alpha
+  // beta
+  int i;
+
+  // one
+  // two
+  /* three */
+}
+
+class J {
+
+  void one(
+
+  ) {}
+
+  void two(
+    // alpha
+  ) {}
+
+  void three(
+    // alpha
+    // beta
+  ) {}
+
+  void four(
+    // alpha
+    // beta
+    /* gamma */
+  ) {}
+
+  void five( // alpha
+  ) {}
+
+  void fiveBis( // alpha
+  ) {
+    int i;
+  }
+
+  void six(/* alpha */) {}
+
+  void seven(
+    /* alpha */
+    /* beta */
+  ) {}
+
+  void eight(
+    /* alpha */
+    // beta
+  ) {}
+
+  void nine(
+  /* alpha */
+  ) {}
+
+  void one(
+    String one
+  ) {}
+
+  void two(
+    String one
+    // alpha
+  ) {}
+
+  void three(
+    String one
+    // alpha
+    // beta
+  ) {}
+
+  void four(
+    // alpha
+    String one
+    // beta
+    /* gamma */
+  ) {}
+
+  void five(String one // alpha
+
+  ) {}
+
+  void six(String one/* alpha */) {}
+
+  void seven(
+    /* alpha */
+    String one
+    /* beta */
+  ) {}
+
+  void eight(
+    /* alpha */
+    String one
+    // beta
+  ) {}
+
+  void nine(String one
+  /* alpha */
+  ) {}
+}
