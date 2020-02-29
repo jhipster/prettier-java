85689033530f8719d280374752192c70b185c7e5
Clement Dessoude <clement.dessoude@gmail.com>
2020-03-01 00:41:58 +0100

    fix: fix stable formating for fields & methods with annotations

--- packages/prettier-plugin-java/test/unit-test/blank_lines/_input.java
+++ packages/prettier-plugin-java/test/unit-test/blank_lines/_input.java
@@ -8,6 +8,13 @@ public class BlankLines {
   public int k = 3;
   public int l = 4;
 
+  // Bug Fix: https://github.com/jhipster/prettier-java/issues/368
+  private String fieldOne;
+  private String fieldTwo;
+  private @Nullable String shouldAddLineBeforeAndAfter;
+  private String fieldThree;
+  private String fieldFour;
+
   public int m = 4;
   public Constructors() {
     this(true);
@@ -46,4 +53,23 @@ public class BlankLines {
 
   }
 
+}
+
+interface BlankLinesInInterfaces {
+    // Bug Fix: https://github.com/jhipster/prettier-java/issues/368
+    String fieldOne;
+    String fieldTwo;
+    @Nullable String shouldAddLineBeforeAndAfter;
+    String fieldThree;
+    String fieldFour;
+
+    private @Nullable String test();
+    private @Nullable static String test();
+    private @Nullable String test();
+    private @Nullable String test();
+    @Nullable
+    private static String test();
+    private @Nullable String test();
+    private static String test();
+    @Nullable String test();
 }
