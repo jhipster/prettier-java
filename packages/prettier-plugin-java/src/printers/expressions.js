56d6c38fd803e1d78da387a3b44ce36f38d675e1
Ling-Chun SO <lingchun.so@gmail.com>
2020-04-20 11:07:38 +0200

    fix: prettier format

--- packages/prettier-plugin-java/src/printers/expressions.js
+++ packages/prettier-plugin-java/src/printers/expressions.js
@@ -293,16 +293,19 @@ class ExpressionsPrettierVisitor {
     const suffixes = [];
 
     if (ctx.primarySuffix !== undefined) {
-
       // edge case: https://github.com/jhipster/prettier-java/issues/381
       let hasFirstInvocationArg = true;
 
-      if(ctx.primarySuffix.length > 1 &&
+      if (
+        ctx.primarySuffix.length > 1 &&
         ctx.primarySuffix[1].children.methodInvocationSuffix &&
-        Object.keys(ctx.primarySuffix[1].children.methodInvocationSuffix[0].children).length === 2) {
+        Object.keys(
+          ctx.primarySuffix[1].children.methodInvocationSuffix[0].children
+        ).length === 2
+      ) {
         hasFirstInvocationArg = false;
       }
-      
+
       if (
         ctx.primarySuffix[0].children.Dot !== undefined &&
         ctx.primaryPrefix[0].children.newExpression !== undefined
@@ -325,7 +328,7 @@ class ExpressionsPrettierVisitor {
         return group(
           rejectAndConcat([
             primaryPrefix,
-            hasFirstInvocationArg?suffixes[0]:indent(suffixes[0]),
+            hasFirstInvocationArg ? suffixes[0] : indent(suffixes[0]),
             indent(rejectAndConcat(suffixes.slice(1)))
           ])
         );
