3805f7f3507f0f0308166ba3cae0181cc2344283
Ling-Chun SO <lingchun.so@gmail.com>
2020-08-11 13:24:01 +0200

    fix: Remove jhipster online due to instability in tests

--- packages/prettier-plugin-java/test/repository-test/jhipster-1-test.js
+++ packages/prettier-plugin-java/test/repository-test/jhipster-1-test.js
@@ -3,7 +3,6 @@ const { resolve } = require("path");
 const { testRepositorySample } = require("../test-utils");
 
 const jhipsterRepository = [
-  "jhipster-online",
   "jhipster-sample-app-microservice",
   "jhipster-sample-app-gateway",
   "jhipster-sample-app-oauth2",
