d4e8e63b2668d432fa4e7287afe5420711e962e1
Clement Dessoude <clement.dessoude@gmail.com>
2021-03-13 10:45:20 +0100

    fix: put space after annotation in UnannClassTypes

---
+++ packages/prettier-plugin-java/test/unit-test/bug-fixes/_output.java
@@ -0,0 +1,13 @@
+class T {
+
+  // Fix for https://github.com/jhipster/prettier-java/issues/453
+  SomeClass.@Valid SomeInnerClass someInnerClass = someClass
+    .getInteractions()
+    .get(0);
+
+  // Fix for https://github.com/jhipster/prettier-java/issues/444
+  void process(
+    Map.@NonNull Entry<String, SkeletonConfiguration> entry,
+    @NonNull Map<String, Object> context
+  ) {}
+}
