465c14f9af948df6786b787c184ed3ceea0d6763
Clement Dessoude <clement.dessoude@gmail.com>
2021-06-19 14:51:22 +0200

    test: increase mocha test timeout

--- packages/java-parser/.mocharc.js
+++ packages/java-parser/.mocharc.js
@@ -1,4 +1,6 @@
 // Enabled Additional validation flows during Parser / Lexer Initialization.
 process.env["prettier-java-development-mode"] = "enabled";
 
-module.exports = {};
+module.exports = {
+  timeout: "5s"
+};
