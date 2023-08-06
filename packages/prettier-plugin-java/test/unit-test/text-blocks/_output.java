6258570ec8f96bc7e7983fed24b5cda8ed5a896a
Jordan Kiesel <jordantkiesel@gmail.com>
2023-08-05 22:39:40 -0600

    feat: adjust text block indentation
    closes #593

--- packages/prettier-plugin-java/test/unit-test/text-blocks/_output.java
+++ packages/prettier-plugin-java/test/unit-test/text-blocks/_output.java
@@ -1,30 +1,50 @@
 public class TextBlock {
 
   void method() {
-    String myTextBlock = """
-         my text
+    String myTextBlock =
+      """
+           my text
 
 
-    sentence\"""
+      sentence\"""
 
-    """;
+      """;
 
     String source =
       """
-                public void print(%s object) {
-                    System.out.println(Objects.toString(object));
-                }
-                """.formatted(
-          type
-        );
+      public void print(%s object) {
+          System.out.println(Objects.toString(object));
+      }
+      """.formatted(type);
 
     String html =
       """
-              <html>\r
-                  <body>\r
-                      <p>Hello, world</p>\r
-                  </body>\r
-              </html>\r
-              """;
+      <html>\r
+          <body>\r
+              <p>Hello, world</p>\r
+          </body>\r
+      </html>\r
+      """;
+
+    System.out.println(
+      // leading comment
+      """
+       abaoeu
+         euaoeu
+      aoeu
+
+       oaeu
+            abc""" // trailing comment
+    );
+
+    System.out.println(
+      """
+       abaoeu
+         euaoeu
+      aoeu
+
+       oaeu
+            abc"""
+    );
   }
 }
