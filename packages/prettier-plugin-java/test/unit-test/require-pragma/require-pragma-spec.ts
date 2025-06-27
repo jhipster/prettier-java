c3660c5aa65f0a33d587e8f369a18a4383993dcf
Craig P. Motlin <cmotlin@gmail.com>
2025-06-27 10:51:28 -0400

    Fix update-test-outputs script to use test-specific prettier options.

--- packages/prettier-plugin-java/test/unit-test/require-pragma/require-pragma-spec.ts
+++ packages/prettier-plugin-java/test/unit-test/require-pragma/require-pragma-spec.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import url from "url";
+import fs from "fs-extra";
 import { testSampleWithOptions } from "../../test-utils.js";
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
@@ -9,10 +10,15 @@ describe("prettier-java: require-pragma option", () => {
     path.resolve(__dirname, "./format-pragma"),
     path.resolve(__dirname, "./prettier-pragma"),
     path.resolve(__dirname, "./invalid-pragma")
-  ].forEach(testFolder =>
+  ].forEach(testFolder => {
+    const optionsPath = path.join(testFolder, "prettier-options.json");
+    const options = fs.existsSync(optionsPath)
+      ? fs.readJsonSync(optionsPath)
+      : { requirePragma: true };
+
     testSampleWithOptions({
       testFolder,
-      prettierOptions: { requirePragma: true }
-    })
-  );
+      prettierOptions: options
+    });
+  });
 });
