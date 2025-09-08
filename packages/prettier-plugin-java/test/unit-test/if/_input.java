87edc7d818dc2908fa877298c2defbca253ca0ea
Jordan Kiesel <jordantkiesel@gmail.com>
2025-09-07 22:41:40 -0600

    fix: eliminate repeated printer function execution from our naively-implemented 'call' helper

--- packages/prettier-plugin-java/test/unit-test/if/_input.java
+++ packages/prettier-plugin-java/test/unit-test/if/_input.java
@@ -40,4 +40,47 @@ public class If {
     }
   }
 
+  void longIfElseChain() {
+    if (1) {
+      // 1
+    } else if (2) {
+      // 2
+    } else if (3) {
+      // 3
+    } else if (4) {
+      // 4
+    } else if (5) {
+      // 5
+    } else if (6) {
+      // 6
+    } else if (7) {
+      // 7
+    } else if (8) {
+      // 8
+    } else if (9) {
+      // 9
+    } else if (10) {
+      // 10
+    } else if (11) {
+      // 11
+    } else if (12) {
+      // 12
+    } else if (13) {
+      // 13
+    } else if (14) {
+      // 14
+    } else if (15) {
+      // 15
+    } else if (16) {
+      // 16
+    } else if (17) {
+      // 17
+    } else if (18) {
+      // 18
+    } else if (19) {
+      // 19
+    } else if (20) {
+      // 20
+    }
+  }
 }
