87cac7ef90ff03625f93091cd2cf71bf8a21a6da
Jordan Kiesel <jordantkiesel@gmail.com>
2026-01-07 21:26:41 -0700

    fix: indent ternary binary expressions in return statements

--- packages/prettier-plugin-java/test/unit-test/conditional-expression/spaces/_output.java
+++ packages/prettier-plugin-java/test/unit-test/conditional-expression/spaces/_output.java
@@ -19,6 +19,53 @@ class ConditionalExpression {
             : thisIsAnotherVeryLongIntegerThatIsEvenLongerThanFirstOne;
     }
 
+    void ternaryOperationThatShouldBreak3() {
+        aaaaaaaaaa &&
+        bbbbbbbbbb &&
+        cccccccccc &&
+        dddddddddd &&
+        eeeeeeeeee &&
+        ffffffffff
+            ? gggggggggg
+            : hhhhhhhhhh;
+        var v =
+            aaaaaaaaaa &&
+            bbbbbbbbbb &&
+            cccccccccc &&
+            dddddddddd &&
+            eeeeeeeeee &&
+            ffffffffff
+                ? gggggggggg
+                : hhhhhhhhhh;
+        v =
+            aaaaaaaaaa &&
+            bbbbbbbbbb &&
+            cccccccccc &&
+            dddddddddd &&
+            eeeeeeeeee &&
+            ffffffffff
+                ? gggggggggg
+                : hhhhhhhhhh;
+        f(
+            aaaaaaaaaa &&
+                bbbbbbbbbb &&
+                cccccccccc &&
+                dddddddddd &&
+                eeeeeeeeee &&
+                ffffffffff
+                ? gggggggggg
+                : hhhhhhhhhh
+        );
+        return aaaaaaaaaa &&
+            bbbbbbbbbb &&
+            cccccccccc &&
+            dddddddddd &&
+            eeeeeeeeee &&
+            ffffffffff
+            ? gggggggggg
+            : hhhhhhhhhh;
+    }
+
     int ternaryOperationThatShouldNotBreak() {
         int a = b ? b : c;
         return b ? b : c;
