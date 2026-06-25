e30853ca0113f84aeeb5737114f53b252dc17f81
Jordan Kiesel <jordantkiesel@gmail.com>
2026-06-25 01:30:15 -0600

    fix: prevent dropping children of parenthesized expressions

--- test/unit-test/comments/expression/_input.java
+++ test/unit-test/comments/expression/_input.java
@@ -7,5 +7,9 @@ class Example {
     a +
       // comment
       (b);
+
+    a = (//
+      b
+    );
   }
 }
--- test/unit-test/comments/expression/_output.java
+++ test/unit-test/comments/expression/_output.java
@@ -7,5 +7,8 @@ class Example {
     a +
       // comment
       b;
+
+    a = //
+      b;
   }
 }
